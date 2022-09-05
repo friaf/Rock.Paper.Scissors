bb2991afb41e36c41a9fb586599c23ed38928e0f
friaf <shovel.irena@gmail.com>
2022-09-05 00:54:01 +0000

    Add js structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,40 @@
+const computerScore = document.getElementByID("computer-score");
+const userScore = document.getElementByID("user-score");
+const computerLose = document.getElementByID("computer-lose");
+const userLose = document.etElementByID("user-lose");
+const computerButton = document.getElementsByClassName("computer-buttom");
+const heroImage = dpcument.getElementByID("hero-image");
+const buttons = document.getElementsByClassName("user-button")
+const choices = ["rock","paper","scissors"];
+
+for ( let button of buttons){
+    button.addEventlistener("click", function(){
+        let userChoice = this.getAttribute("data-choice");
+        playGame(userChoice);
+    })
+    
+}
+
+
+function playGame(userChoice) {
+
+    let computerChoice = Math.floor(Math.random() * 3);
+    let result = checkWinner(choices[computerChoice], choices[userChoice])
+
+    updateScores(result);
+
+}
+
+
+
+
+
+
+function updateScores()
+
+function computerChoice()
+
+function imageChange()
+
+function scoreUpdate()
 
